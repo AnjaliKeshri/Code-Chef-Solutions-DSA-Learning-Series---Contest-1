4b3bbb0815f21335ec7be3ab9ae727a721097825
WhiiteWalker <anjali.14238@gmail.com>
2021-10-21 20:30:27 +0530

    FLOW007

---
+++ TEST.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int a;
+    while(true)
+    {
+        cin>>a;
+        if(a==42)
+            break;
+        cout<<a<<"\n";
+    }
+	  
+	return 0;
+}
